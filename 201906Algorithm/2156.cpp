dd353af039f5cb319190ae00d0b5f5b964116b46
acupofmilktea <mtoc227@gmail.com>
2019-06-05 23:49:20 +0900

    dp

---
+++ 201906Algorithm/2156.cpp
@@ -0,0 +1,26 @@
+//
+// Created by az91t on 2019-06-05.
+//
+
+#include <iostream>
+#define MAX 10002
+using namespace std;
+int main() {
+    int n;
+    cin>>n;
+    int cost[MAX]={0,}, dp[MAX]={0,};
+    for(int i=1; i<=n; i++) {
+        cin>>cost[i];
+        dp[i]=cost[i];
+    }
+    if(n==1) {
+        cout<<cost[1];
+        return 0;
+    }
+    dp[2]=dp[1]+cost[2];
+    for(int i=3; i<=n; i++) {
+        dp[i]=max(dp[i-3]+cost[i-1]+cost[i], max(dp[i-1], dp[i-2]+cost[i]));
+    }
+    cout<<dp[n];
+    return 0;
+}
---
+++ 201906Algorithm/cmake-build-debug/CMakeFiles/201906Algorithm.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/201906Algorithm.dir/main.cpp.obj"
+  "201906Algorithm.pdb"
+  "201906Algorithm.exe"
+  "201906Algorithm.exe.manifest"
+  "lib201906Algorithm.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/201906Algorithm.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
